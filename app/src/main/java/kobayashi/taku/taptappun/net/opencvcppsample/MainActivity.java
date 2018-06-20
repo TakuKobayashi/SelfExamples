2825c4e91f036023691826b9108ef3a8458cebd4
TakuKobayshi <>
2018-06-20 18:59:26 +0900

    サンプルの画像を追加して表示させてみた

--- app/src/main/java/kobayashi/taku/taptappun/net/opencvcppsample/MainActivity.java
+++ app/src/main/java/kobayashi/taku/taptappun/net/opencvcppsample/MainActivity.java
@@ -1,8 +1,15 @@
 package kobayashi.taku.taptappun.net.opencvcppsample;
 
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +26,16 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+        try {
+            InputStream ims = getAssets().open("sample_sprite.jpg");
+            Bitmap bitmap = BitmapFactory.decodeStream(ims);
+
+            ImageView image = (ImageView) findViewById(R.id.sample_image);
+            image.setImageBitmap(bitmap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
